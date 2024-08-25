8912d31f253bc381524862197f2d0954cd3ec232
dakson99 <darko99.mikavica@gmail.com>
2024-08-25 12:50:41 +0200

    Updated weather display logic: set temperature, description, and additional details dynamically; added error handling for invalid city searches

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let clouds = document.getElementById('clouds');
 let humidity = document.getElementById('humidity');
 let pressuer = document.getElementById('pressure');
 let form = document.querySelector('form');
+let main = document.querySelector('main');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -26,6 +27,28 @@ const searchWeather = () => {
                 city.querySelector('figcaption').innerHTML = data.name;
                 city.querySelector('img').src = 'https://flagsapi.com/' + data.sys.country + '/shiny/32.png';
 
+                temperature.querySelector('img').src = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@4x.png';
+                temperature.querySelector('figcaption span').innerHTML =
+                    data.main.temp;
+                description.innerText = data.weather[0].description;
+                clouds.innerText = data.clouds.all;
+                humidity.innerText = data.main.humidity;
+                pressuer.innerText = data.main.pressuer;
+            } else {
+                // false
+                main.classList.add('error');
+                setTimeout(() => {
+                    main.classList.remove('error');
+                }, 1000);
             }
+
+            valueSearch.value = '';
         })
 }
+
+const initApp = () => {
+    valueSearch.value = 'Washington';
+    searchWeather();
+}
+
+initApp();
